e38dc5d04af7c47b1a88f4f49c0cdf184d6ca3c5
calaandrea <cala.andrea@protonmail.com>
2026-04-05 21:16:35 -0500

    modifica

--- assets/js/lifeboat.js
+++ assets/js/lifeboat.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sequence = [
     '2026_Lifeboat00001.jpg',
-    null,
     '2026_Lifeboat00002.jpg',
     '2026_Lifeboat00003.jpg',
     '2026_Lifeboat00004.jpg',
     '2026_Lifeboat00005.jpg',
-    null,
     '2026_Lifeboat00006.jpg',
-    null,
     '2026_Lifeboat00007.jpg',
     '2026_Lifeboat00008.jpg',
     '2026_Lifeboat00009.jpg',
